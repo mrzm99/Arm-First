98e040ff5a2285a66bb27eac0e29bdd471c92c65
mrzm99 <mrzu.cpp@gmail.com>
2025-05-02 20:13:20 +0900

    add: set/get register function

--- utility/utility.h
+++ utility/utility.h
@@ -10,4 +10,17 @@
 
 #include <stdint.h>
 
+ /*--------------------------------------------------------------------------------------*/
+ /*! @brief operate register/memory
+  */
+ #define set_word(base, offset, val)    (*((volatile uint32_t *)(base + offset)) = (uint32_t)val)
+ #define set_hword(base, offset, val)   (*((volatile uint16_t *)(base + offset)) = (uint16_t)val)
+ #define set_byte(base, offset, val)    (*((volatile uint8_t *)(base + offset)) = (uint8_t)val)
+ #define get_word(base, offset)         (*((volatile uint32_t *)(base + offset)))
+ #define get_hword(base, offset)        (*((volatile uint16_t *)(base + offset)))
+ #define get_byte(base, offset)         (*((volatile uint8_t *)(base + offseet)))
+
+ #define set_bit(base, offset, bit)     (*((volatile uint32_t *)(base + offset)) |= (1<<(bit)))
+ #define clr_bit(base, offset, bit)     (*((volatile uint32_t *)(base + offset)) &= (~(1<<bit)))
+
 extern void busy_wait(uint32_t cnt);
