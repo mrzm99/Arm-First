38f081750dda7c49ee284eaec65685dd2632feb9
mrzm99 <mrzu.cpp@gmail.com>
2025-08-13 19:44:16 +0900

    add: test mailbox

--- application/main.c
+++ application/main.c
@@ -13,7 +13,14 @@
 #include "task_manage.h"
 #include "task_sync.h"
 #include "cyc_hdlr.h"
+#include "mailbox.h"
+#include "mem_alloc.h"
 #include <stdio.h>
+
+typedef struct {
+    T_MSG   msghd;
+    UW      lvl;
+} msg_t;
 
 static void test_cyc(void)
 {
@@ -24,23 +31,67 @@ static void test_cyc(void)
     port_drv_set_pin_lvl(PORTB4, set_val);
 }
 
+static void test1(void)
+{
+    uint32_t lvl;
+    msg_t *msg;
+
+    while (1) {
+        if (rcv_mbx(0, (T_MSG**)&msg, TMO_FEVR) == E_OK) {
+            lvl = msg->lvl;
+            port_drv_set_pin_lvl(PORTB4, lvl);
+//            mem_free(msg);
+        }  
+    } 
+}
+
+static void test2(void)
+{
+    uint32_t lvl = 0;
+    msg_t *msg;
+
+    while (1) {
+        msg = (msg_t*)mem_alloc(sizeof(msg_t)); 
+        msg->lvl = lvl;
+        lvl = ~lvl & 1;
+        snd_mbx(0, (T_MSG*)msg);
+        dly_tsk(1000);
+    }
+}
+
 /*--------------------------------------------------------------------------------------*/
 /*! @brief  app main
  */
 void app_main(void)
 {
-    T_CCYC ccyc;
-    ccyc.cycatr = TA_HLANG|TA_STA;
-    ccyc.exinf = NULL;
-    ccyc.cychdr = test_cyc;
-    ccyc.cyctime = 1000;
-    ccyc.cycphs = 0;
     port_drv_set_pin_func(PORTB4, PORTB4_OUTPUT, PORT_LVL_HIGH, 0, 0, 0);
 
-    if (cre_cyc(0, &ccyc) < 0) {
+    T_CTSK init_ctsk;
+    init_ctsk.tskatr = TA_HLANG|TA_ACT;
+    init_ctsk.exinf = NULL;
+    init_ctsk.task = test1;
+    init_ctsk.itskpri = 0;
+    init_ctsk.stksz = 256;
+    init_ctsk.stk = NULL;
+    if (cre_tsk(2, &init_ctsk)) {
         while (1);
     }
-    
+    init_ctsk.tskatr = TA_HLANG|TA_ACT;
+    init_ctsk.exinf = NULL;
+    init_ctsk.task = test2;
+    init_ctsk.itskpri = 1;
+    init_ctsk.stksz = 256;
+    init_ctsk.stk = NULL;
+    if (cre_tsk(3, &init_ctsk)) {
+        while (1);
+    }
+
+    T_CMBX cmbx;
+    cmbx.mbxatr = TA_TFIFO;
+    if (cre_mbx(0, &cmbx)) {
+        while (1);
+    }
+   
     // end app main
     slp_tsk(TMO_FEVR);
 
